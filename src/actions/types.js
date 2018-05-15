9b3c16f8fec4649364ca24b79a6d74c792016e9a
bhaveshc20 <bhaveshc789@gmail.com>
2018-05-14 22:15:07 -0700

    auth actions for firebase and twilio

--- src/actions/types.js
+++ src/actions/types.js
@@ -5,3 +5,12 @@ export const CREATE_USER_FAIL = 'create_user_fail';
 export const LOGIN_USER_SUCCESS = 'login_user_success';
 export const LOGIN_USER_FAIL = 'login_user_fail';
 export const LOGIN_USER_START = 'login_user_start';
+export const LOGOUT_USER_SUCCESS = 'logout_user_success';
+
+export const COIN_CHECKED = 'coin_changed';
+export const COIN_UNCHECKED = 'coin_unchecked';
+export const COINS_SAVED = 'coins_saved';
+export const COINS_FETCHED = 'coins_fetched';
+export const ASSETS_CHANGED = 'assets_changed';
+export const ASSETS_SAVED = 'assets_saved';
+export const PORTFOLIO_FETCH = 'portfolio_fetch';
