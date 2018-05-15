2c2dd1229650f1b520383cc5ad724abe6a1e7140
Arangeen <arangeen@mail.sfsu.edu>
2018-05-14 21:32:12 -0700

    auth and intro screen finished

--- src/components/IntroScreen.js
+++ src/components/IntroScreen.js
@@ -1,53 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, StatusBar } from 'react-native';
-import { LinearGradient } from 'expo';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-import { Font } from 'expo';
-import LoginForm from './LoginForm'
 
-import LOGO from '../../assets/moonshot.gif';
+import LOGO from '../../assets/folio.png';
 
-
-export default class IntroScreen extends React.Component {
-
+export default class IntroScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        {/* <StatusBar
-          barStyle="light-content"
-          backgroundColor="#6a51ae"
-        /> */}
         <View style={styles.introContainer}>
-          <Image
-            source={LOGO}
-            style={styles.logo}
-          />
-          <Text style={{
-            fontSize: 30,
-            fontWeight: '700',
-            color: 'white'}}>MOONSHOT       </Text>
-          
+          <Image source={LOGO} style={styles.logo} />
         </View>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            onPress={Actions.loginUser}
-          >
+          <TouchableOpacity onPress={Actions.phoneAuth}>
+            <Text style={styles.button}> Login with Phone Number </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ height: 1 }} />
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={Actions.loginUser}>
             <Text style={styles.button}> Login </Text>
           </TouchableOpacity>
-        <TouchableOpacity
-            onPress={Actions.createUser}
-          >
-            <Text style={styles.button}> Signup </Text>
-          </TouchableOpacity> 
+          <TouchableOpacity onPress={Actions.createUser}>
+            <Text style={styles.button}> Sign Up </Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2A033E',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -59,20 +45,19 @@ const styles = StyleSheet.create({
   logo: {
     resizeMode: 'cover',
     width: 400,
-    height: 300,
-    marginBottom: 10
-    
+    height: 400,
+    marginBottom: 10,
   },
   buttonContainer: {
     flexDirection: 'row',
     height: 60,
-    width: '100%',    
+    width: '100%',
     justifyContent: 'space-around',
     padding: 15,
-    backgroundColor: 'rgba(92, 99,216, 1)',
+    backgroundColor: '#FF5636',
   },
   button: {
     fontSize: 20,
     color: 'white',
-  }
+  },
 });
